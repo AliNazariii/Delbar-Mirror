72718112b4d2a5a230738a50cf152864512948c1
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2020-02-12 12:55:14 +0330

    fa traslation initiated weather added

--- MagicMirror/config/config.js
+++ MagicMirror/config/config.js
@@ -20,7 +20,26 @@ var config = {
 		{
 			module: "aiclientdebugger",
 			position: "bottom_right"
-		}
+		},
+		{
+			module: "currentweather",
+			position: "top_right",
+			config: {
+				location: "Tehran",
+				locationID: "112931",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "178f407a9c21114f67c8f08550d941aa"
+			}
+		},
+		{
+			module: "weatherforecast",
+			position: "top_right",
+			header: "Weather Forecast",
+			config: {
+				location: "Tehran",
+				locationID: "112931",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "178f407a9c21114f67c8f08550d941aa"
+			}
+		},
 	],
 
 	electronOptions: {
